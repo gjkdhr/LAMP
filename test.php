2f61a50c1ae829f0bc1707f8743a79c53a9829a4
gjkdhr <gjkdhr@163.com>
2015-11-23 09:41:59 +0800

    the test script in mysql and php

--- test.php
+++ test.php
@@ -1,14 +1,10 @@
 <?php
 $mem = new Memcache;
 $mem->connect("127.0.0.1", 11211)  or die("Could not connect");
-
 $version = $mem->getVersion();
 echo "Server's version: ".$version."<br/>\n";
-
 $mem->set('testkey', 'Hello World', 0, 600) or die("Failed to save data at the memcached server");
 echo "Store data in the cache (data will expire in 600 seconds)<br/>\n";
-
 $get_result = $mem->get('testkey');
 echo "$get_result is from memcached server.";         
 ?>
-
